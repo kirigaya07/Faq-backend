dbfc00daddb7d4bf31d95a571e8ff30bfcd7e503
kirigaya07 <crimsomemoon7@gmail.com>
2025-01-31 23:02:02 +0530

    Db connection initiated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -7,6 +8,15 @@ const app = express();
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Failed to start server:", error);
+  }
+}
+
+startServer();
